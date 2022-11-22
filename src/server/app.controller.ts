da4f20932abfa7e7240111ed4bf98391cc57acef
Gustavo Souza <til_gsouza@uolinc.com>
2022-11-22 08:46:00 -0300

    Add Swagger

--- src/server/app.controller.ts
+++ src/server/app.controller.ts
@@ -1,16 +1,25 @@
 import { Controller, Get, Header, Logger, Param, Req } from '@nestjs/common';
+import { ApiParam, ApiProperty, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { validateContentType } from './validator/validator';
 
-@Controller()
+@ApiTags('Repositories')
+@Controller('Repositories')
 export class AppController {
   private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
   @Get(':username')
-  async getRepos(@Param() params, @Req() request: Request) {
+  @ApiParam({name: 'username', required: true, description: 'Github username', schema: {type: 'string'}})
+  async getRepos(
+    @Param() params: ReposParams,
+    @Req() request: Request) {
     this.logger.warn(`New request for ${params.username}`)
     validateContentType(request.headers['accept'])
     return await this.appService.getRepo(params.username)
   }
+}
+
+interface ReposParams {
+  username: string
 }
